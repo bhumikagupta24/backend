6c91cc909ac03627807ca09ec164efb923b5aecc
Bhumika Gupta <minig2429@gmail.com~>
2025-05-25 00:10:15 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
+const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
+const OpenAI = require("openai")
+
+connectToDb();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+const openai = new OpenAI({ apiKey: 'process.env.OPENAI_API_KEY' });
+
+
+
+app.post("/chat", async (req, res) => {
+  try {
+    const { message } = req.body;
+    const response = await openai.chat.completions.create({
+      model: "gpt-4",
+      messages: [{ role: "user", content: message }],
+    });
+
+    res.json({ reply: response.choices[0].message.content });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
+
+module.exports = app;
+
